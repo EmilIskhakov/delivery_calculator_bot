7823a5ac0e237267a167c0e6efca9ad2851bdabe
EmilIskhakov <125988458+EmilIskhakov@users.noreply.github.com>
2025-02-09 23:50:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,27 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.storage.memory import MemoryStorage
-from dotenv import load_dotenv
 import os
 
-
 API_TOKEN = '7668930405:AAHKKrLILpoQ5x8h9TW0Ttou2KCikqxEOD8'
-
 
 # Инициализация бота и хранилища состояний
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)  # Создаем диспетчер
 
-# Создание кнопки для открытия веб-приложения
+# Обработчик команды /start
 @dp.message(Command("start"))
 async def start(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(
-            text="Открыть форму",
-            web_app=types.WebAppInfo(url="https://delivery-calculator-1.onrender.com")  # Замените на ваш URL
-        )]
+        [  # Единственный ряд с одной кнопкой
+            InlineKeyboardButton(
+                text="Открыть форму",
+                web_app=types.WebAppInfo(url="https://delivery-calculator-1.onrender.com")  # URL веб-приложения
+            )
+        ]
     ])
+    
     await message.reply("Нажмите кнопку, чтобы открыть форму:", reply_markup=keyboard)
 
 # Запуск бота
