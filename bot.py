b0151145a8c2bcb11188deeac51ad9a8df4f36e8
EmilIskhakov <125988458+EmilIskhakov@users.noreply.github.com>
2025-02-10 00:05:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,45 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, BotCommand
-from aiogram.fsm.storage.memory import MemoryStorage
 import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 
 API_TOKEN = '7668930405:AAHKKrLILpoQ5x8h9TW0Ttou2KCikqxEOD8'
+WEBAPP_HOST = '0.0.0.0'  # IP-адрес сервера
+WEBAPP_PORT = int(os.getenv('PORT', 8080))  # Порт, указанный в Render.com
+WEBHOOK_PATH = f"/webhook/{API_TOKEN}"  # Путь для вебхука
+BASE_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}"  # URL вашего сервера
 
-# Инициализация бота и хранилища состояний
-bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage)  # Создаем диспетчер
+bot = Bot(token=API_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
 
-# Настройка команды /start
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.reply("Привет! Чтобы открыть форму, используйте кнопку ниже.")
-
-# Установка постоянного меню с кнопкой
-async def set_main_menu():
-    commands = [
-        BotCommand(command="open_form", description="Открыть форму"),  # Команда для кнопки
-    ]
-    await bot.set_my_commands(commands)
-
-# Обработчик для команды /open_form
-@dp.message(Command("open_form"))
-async def open_form(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(
             text="Открыть форму",
-            web_app=types.WebAppInfo(url="https://delivery-calculator-1.onrender.com")  # URL веб-приложения
+            web_app=types.WebAppInfo(url="https://delivery-calculator-1.onrender.com")
         )]
     ])
     await message.reply("Нажмите кнопку, чтобы открыть форму:", reply_markup=keyboard)
 
-# Запуск бота
+async def on_startup(app):
+    webhook_url = BASE_URL + WEBHOOK_PATH
+    await bot.set_webhook(url=webhook_url)
+
+async def on_shutdown(app):
+    await bot.delete_webhook()
+
 async def main():
-    await set_main_menu()  # Устанавливаем главное меню
-    await dp.start_polling(bot)
+    app = web.Application()
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, WEBAPP_HOST, WEBAPP_PORT)
+    await site.start()
 
 if __name__ == '__main__':
     asyncio.run(main())
